94491a10a426a4af846a2002007a21c3f68127ab
danielnaiff <daniel.naiff@yahoo.com.br>
2023-04-02 20:05:07 -0300

    Create 1º_tarefaextra_niveis_de_quantização.py

---
+++ 1º_Semester/Computer_Systems/1º_tarefaextra_niveis_de_quantização.py
@@ -0,0 +1,11 @@
+import itertools
+import math
+import itertools 
+
+
+q = int(input("Digite o número de níveis de quantização:  "))
+bits = int(math.log2(q))
+print(f"Número de bits: {bits}")
+v = [0, 1]
+com_set = list(itertools.product(v, repeat = bits))
+print(com_set)
