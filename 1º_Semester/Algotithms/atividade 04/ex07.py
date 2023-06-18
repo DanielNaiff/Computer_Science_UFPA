6f5d2ce587eccb618a799a972a5417215dd4e0c4
danielnaiff <daniel.naiff@yahoo.com.br>
2023-06-18 16:45:55 -0300

    exercise

--- 1º_Semester/Algotithms/atividade 04/ex07.py
+++ 1º_Semester/Algotithms/atividade 04/ex07.py
@@ -1,14 +1,36 @@
 from random import randint
 mat = []
-soma_linha = soma_coluna = 0
 for l in range(5):
     linha = []
-    for c in range(5):
-        linha += [randint(0,25)]
+    soma_linha = 0
 
-    matriz += [linha]
-print("\033[1;32mMatriz:\033[m")
+    for c in range(5):
+        
+        linha += [randint(0,25)]
+        soma_linha += linha[c]
+        if soma_linha % 2 == 0:
+            paridade = 'Par'
+        else:
+            paridade = 'Impar'
+
+    mat += [linha]
+    print(f"\033[1;32mSoma da {l+1}º linha:\033[m",soma_linha,f"\033[1;34m- {paridade}\033[m")
+
+print(f"\033[1;35m-=\033[m"*30)
+
+for l in range(5):
+    soma_coluna = 0
+    for c in range(5):
+        soma_coluna += mat[c][l]
+        if soma_coluna % 2 == 0:
+            paridade = 'Par'
+        else:
+            paridade = 'Impar'
+    print(f"\033[1;33mSoma da {l+1}º coluna:\033[m",soma_coluna,f"\033[1;34m- {paridade}\033[m")
+
+print("\033[1;36m\nMatriz:\033[m")
+
 for l in range(5):
     for c in range(5):
-        print(f"[{matriz[l][c]:^5}]",end='')
+        print(f"[{mat[l][c]:^5}]",end='')
     print()
