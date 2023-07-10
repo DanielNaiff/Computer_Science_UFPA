70bf3944c57a1a5b5a865fcfb2b049b63362328a
danielnaiff <daniel.naiff@yahoo.com.br>
2023-07-10 19:22:11 -0300

    Create Exemplo.py

---
+++ 1º_Semester/Algotithms/class11/Exemplo.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+@dataclass
+class RegEstudante:
+    Nome: str
+    Matricula: str
+    Nota: list
+    Media: float
+
+Aluno = [None]*4
+for i in range(4):
+    Aluno[i] = RegEstudante('','',[0.0,0.0,0.0,0.0,],0.0)
+for i in range(4):
+    Aluno[i].Nome = input("Digite o nome: ")
+for j in range(3):
+    Aluno[i].Nota[j] = float(input(f"Nota{j+1}:"))
+    Aluno[i].Media = Aluno[i].Media + Aluno[i].Nota[j]*(j+1)/10
+print(f"{Aluno[i].Nome} tem média = {Aluno[i].Media}")
