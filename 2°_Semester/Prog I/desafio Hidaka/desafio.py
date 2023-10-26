e31474c69ef7a8d253208bce72117cfdf3114279
danielnaiff <daniel.naiff@yahoo.com.br>
2023-10-26 20:59:00 -0300

    desafio

---
+++ 2°_Semester/Prog I/desafio Hidaka/desafio.py
@@ -0,0 +1,32 @@
+dic = {
+  "nome": "",
+  "nascimento": "",
+  "sexo": "",
+  "CPF": "",
+  "Rg": ""
+}
+lista_dicionarios = []
+import desafio_funcoes as f
+
+while True:
+    print("1 para cadastrar os dados 2 para mostrar os dicionario ou aperte qualquer tecla para finalizar")
+    opc = input("Digite uma opção:")
+
+    if opc == "1":
+        dic["nome"] = f.cadastro_nome()
+        dic["nascimento"] = f.cadastro_nascimento()
+        dic["sexo"] = f.cadastro_sexo()
+        dic["CPF"] = f.cadastro_cpf()
+        dic["Rg"] = f.cadastro_rg()
+        lista_dicionarios.append(dic.copy())  # Copia o dicionário para evitar referência direta
+
+    elif opc == "2":
+        try:
+            f.listar_dicionario(lista_dicionarios)
+            if not lista_dicionarios:
+                raise Exception("O sistema ainda não possui nenhum dado cadastrado")
+        except Exception as e:
+            print(e)
+
+    else:
+        break
---
+++ 2°_Semester/Prog I/desafio Hidaka/desafio_funcoes.py
@@ -0,0 +1,88 @@
+dic = {}
+
+def cadastro_nome():
+  while True:
+    try:
+      nome = input("Digite o seu nome:")
+
+      if nome.isdigit():
+        raise Exception("Nome inválido. Tente novamente")
+      
+    except Exception as e:
+      print(e)
+
+    else:
+      return nome
+      
+
+def cadastro_nascimento():
+  while True:
+    try:
+      nascimento = input("Digite o seu nascimento(xxxx):")
+
+      if not nascimento.isdigit():
+        raise Exception("Ano inválido. Tente novamente")
+      elif int(nascimento) < 1903:
+        raise Exception("Digite apenas número. Tente novamente")
+      
+    except Exception as e:
+      print(e)
+
+    else:
+      return nascimento
+
+def cadastro_sexo():
+  while True:
+    try:
+      sexo = input("Digite o seu sexo(M/F):")
+
+      if sexo not in ["F", "M", 'f', 'm']:
+        raise Exception("Digite um sexo válido")
+      
+    except Exception as e:
+      print(e)
+
+    else:
+      return sexo.upper()
+
+def cadastro_cpf():
+  while True:
+    try:
+      cpf = input("Digite o seu cpf:")
+
+      if not cpf.isdigit():
+        raise Exception("Digite um cpf válido. Digite novamente")
+
+      elif len(cpf) < 11:
+        raise Exception("CPF muito curto. Digite novamente")
+      
+    except Exception as e:
+      print(e)
+
+    else:
+      return cpf
+
+def cadastro_rg():
+  while True:
+    try:
+      rg = input("Digite o seu Rg:")
+
+      if not rg.isdigit():
+        raise Exception("Digite um Rg válido. Digite novamente")
+
+      elif len(rg) < 7:
+        raise Exception("Rg muito curto. Digite novamente")
+      
+    except Exception as e:
+      print(e)
+
+    else:
+      return rg
+
+def listar_dicionario(lista):
+  for dicionario in lista:
+    print(f"-=-Dicionario-=-")
+    for chave, valor in dicionario.items():
+      print(f"{chave}:{valor}")
+      print("-=-=-=-=-=-=-=-=-=-=-=")
+  return
