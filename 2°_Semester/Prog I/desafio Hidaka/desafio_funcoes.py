c200cbbd92099e9ca39263e941df20defaffc026
danielnaiff <daniel.naiff@yahoo.com.br>
2023-10-26 22:04:33 -0300

    .

--- 2°_Semester/Prog I/desafio Hidaka/desafio_funcoes.py
+++ 2°_Semester/Prog I/desafio Hidaka/desafio_funcoes.py
@@ -1,38 +1,53 @@
+def validar_nome(nome):
+    if not nome.isalpha():
+        raise ValueError("Nome inválido. Deve conter apenas letras.")
+
+def validar_ano(ano):
+    if not ano.isdigit() or int(ano) < 1903:
+        raise ValueError("Ano inválido. Deve ser igual ou maior que 1903.")
+
+def validar_sexo(sexo):
+    if sexo.upper() not in ["M", "F"]:
+        raise ValueError("Sexo inválido. Digite 'M' ou 'F'.")
+
 def validar_cpf(cpf):
     if not cpf.isdigit() or len(cpf) < 11:
-        raise Exception("CPF inválido. Digite novamente.")
+        raise ValueError("CPF inválido. Deve conter apenas números e ter no mínimo 11 dígitos.")
 
 def validar_rg(rg):
     if not rg.isdigit() or len(rg) < 7:
-        raise Exception("RG inválido. Digite novamente.")
+        raise ValueError("RG inválido. Deve conter apenas números e ter no mínimo 7 dígitos.")
 
-def obter_entrada(mensagem, validacao):
+def obter_entrada(mensagem, validacao, mensagem_erro):
     while True:
         entrada = input(mensagem)
         try:
             validacao(entrada)
             return entrada
-        except Exception as e:
-            print(e)
+        except ValueError as ve:
+            print(mensagem_erro)
 
 def cadastro_nome():
-    return obter_entrada("Digite o seu nome:", lambda x: not x.isdigit())
+    mensagem = "Digite o seu nome: "
+    mensagem_erro = "Nome inválido. Deve conter apenas letras."
+    return obter_entrada(mensagem, validar_nome, mensagem_erro)
 
 def cadastro_nascimento():
-    return obter_entrada("Digite o seu nascimento (xxxx):", lambda x: x.isdigit() and int(x) >= 1903)
+    mensagem = "Digite o seu nascimento (xxxx): "
+    mensagem_erro = "Ano inválido. Deve ser igual ou maior que 1903 e conter apenas números."
+    return obter_entrada(mensagem, validar_ano, mensagem_erro)
 
 def cadastro_sexo():
-    return obter_entrada("Digite o seu sexo (M/F):", lambda x: x.upper() in ["M", "F"])
+    mensagem = "Digite o seu sexo (M/F): "
+    mensagem_erro = "Sexo inválido. Digite 'M' ou 'F'."
+    return obter_entrada(mensagem, validar_sexo, mensagem_erro)
 
 def cadastro_cpf():
-    return obter_entrada("Digite o seu CPF:", validar_cpf)
+    mensagem = "Digite o seu CPF: "
+    mensagem_erro = "CPF inválido. Deve conter apenas números e ter no mínimo 11 dígitos."
+    return obter_entrada(mensagem, validar_cpf, mensagem_erro)
 
 def cadastro_rg():
-    return obter_entrada("Digite o seu RG:", validar_rg)
-
-def listar_dicionario(lista):
-    for dicionario in lista:
-        print("-=- Dicionário -=-")
-        for chave, valor in dicionario.items():
-            print(f"{chave}: {valor}")
-        print("-=-=-=-=-=-=-=-=-")
+    mensagem = "Digite o seu RG: "
+    mensagem_erro = "RG inválido. Deve conter apenas números e ter no mínimo 7 dígitos."
+    return obter_entrada(mensagem, validar_rg, mensagem_erro)
