6270f91869229aea2d3bc27246875d5312f989c3
danielnaiff <daniel.naiff@yahoo.com.br>
2023-12-03 21:50:26 -0300

    study

--- 2°_Semester/Prog I/Expressões regulares/aula5(grupos e retrovisores).py
+++ 2°_Semester/Prog I/Expressões regulares/aula5(grupos e retrovisores).py
@@ -8,16 +8,13 @@ from pprint import pprint
 texto = '''
 <p>Frase 1</p> <p>Eita</p> <p>Qualquer frase</p> <div>1</div> 
 '''
+print(re.findall(r'(<([pdiv]{1,3})>.+?<\/\2>)', texto))
+print(re.findall(r'(<([pdiv]{1,3})>(.+?)<\/\2>)', texto))
 
-# cpf = 'a 147.852.963-12 a'
-# print(re.findall(r'((?:[0-9]{3}\.){2}[0-9]{3}-[0-9]{2})', cpf))
+cpf = '147.852.963-12'
 
-# tags = re.findall(r'<([dpiv]{1,3})>(.+?)<\/\1>', texto)
-# tags = re.findall(r'<(?P<tag>[dpiv]{1,3})>(.+?)<\/(?P=tag)>', texto)
-# pprint(tags)
+print(re.findall(r'[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}', cpf))
+print(re.findall(r'((?:[0-9]{3}\.){2}[0-9]{3}\-[0-9])', cpf))
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
 
-print(re.sub(r'(<(.+?)>)(.+?)(<\/\2>)', r'\1 MAIS \3 COISAS \4', texto))
-
-# # for tag in tags:
-# #     um, dois, tres = tag
-# #     print(tres)
+print(re.sub(r'(<(.+?)>)(.+?)(<\/\2>)', r'\1 \3 \4', texto))
