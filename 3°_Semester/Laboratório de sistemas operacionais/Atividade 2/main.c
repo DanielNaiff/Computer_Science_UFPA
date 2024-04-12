f0a74a7ec7cc355b4c5d0b50a6ac7fb7368b6f57
Daniel Naiff <68018516+DanielNaiff@users.noreply.github.com>
2024-04-11 23:23:07 -0300

    Create main.c

---
+++ 3°_Semester/Laboratório de sistemas operacionais/Atividade 2/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int numeroProcessos;
+
+  printf("Quanto processos voce quer criar? ");
+  scanf("%d", &numeroProcessos);
+
+  if (numeroProcessos <= 0) {
+    printf("\nO numero deve ser positivo\n");
+    return 1;
+  }
+
+  for (int i = 0; i < numeroProcessos - 1; i++) {
+    pid_t pid;
+    pid = fork();
+
+    if (pid < 0) {
+      printf("Fork falhou.\n");
+      return 1;
+    }
+      
+    else if (pid == 0) {    
+      execl("./helloWorld", "./helloWorld", NULL); 
+      printf("failed.\n");
+      return 0;
+    }
+  }
+  
+  for (int i = 0; i < numeroProcessos; i++) {
+    wait(NULL);
+  }
+  execl("./helloWorld", "./helloWorld", NULL);
+  printf("Os processos terminaram.\n");
+
+  return 0;
+}
